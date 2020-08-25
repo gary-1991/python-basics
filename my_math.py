3ca8337a506a964fb23753d0b205c0cc57fac738
Gaurov Ojha <gauojha@publicisgroupe.net>
2020-08-25 19:13:32 +0100

    my first commit

---
+++ my_math.py
@@ -0,0 +1,23 @@
+def add(a,b):
+    # return a+b
+    x = a+b
+    return x
+
+def multiply(a,b):
+    x=a*b
+    return x
+
+def divide(a,b):
+    x=a/b
+    return x
+
+def subtract(a,b):
+    x=a-b
+    return x
+
+
+
+
+
+
+    
